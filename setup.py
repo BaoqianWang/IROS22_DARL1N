57b1f3f6f2fb6b324a0396b7122b91b3947d904d
BaoqianWang <you@example.com>
2022-03-01 10:05:36 -0800

    1

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='aaai_darl1n',
+setup(name='iros22_darl1n',
       version='0.0.1',
       description='Distributed multi-agent Reinforcement Learning with One-Hope Neighbors (DARL1N)',
-      author='Anonymous',
+      author='BaoqianWang',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False
